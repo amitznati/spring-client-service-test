070680015457b41394a621e2db17722ff8c0c209
AmitZnati <amitznati@gmail.com>
2018-05-10 22:39:25 +0200

    cosmetic

--- demo-client/src/main/java/com/example/demo/DemoClientApplication.java
+++ demo-client/src/main/java/com/example/demo/DemoClientApplication.java
@@ -1,9 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class DemoClientApplication {
@@ -18,4 +27,49 @@ public class DemoClientApplication {
 	}
 	
 	
+}
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+class Foo {
+
+	private Long id;
+	private String name;
+	private Bar bar;
+	
+}
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+class Bar {
+
+	private Long id;
+	private String name;
+	private Foo foo;
+	
+}
+
+@RestController
+class FooBarController {
+
+	@Autowired
+	RestTemplate restTemplate;
+	
+	String barUrl = "http://localhost:8401/bars";
+	String fooUrl = "http://localhost:8401/foos";
+	
+	@PostMapping("/api/bars")
+	public Bar addBar(@RequestBody Bar bar,
+			@RequestParam("foo_id") int driverId){
+		Bar retVal = null;
+		try {
+			retVal = restTemplate.postForObject(barUrl, bar, Bar.class);
+			
+		}catch (Exception e) {
+			throw e;
+		}
+		return retVal;
+	}
 }
--- demo-service/src/main/java/com/example/demo/DemoServiceApplication.java
+++ demo-service/src/main/java/com/example/demo/DemoServiceApplication.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -21,6 +25,27 @@ import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class DemoServiceApplication {
+
+	@Autowired
+	BarRepository br;
+	
+	@Autowired
+	FooRepository fr;
+	
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			addData();
+			
+		};
+	}
+	
+	private void addData() {
+		
+		fr.save(new Foo(null,"foo1",null));
+		br.save(new Bar(null,"bar1",null));
+		
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServiceApplication.class, args);
@@ -50,7 +75,7 @@ interface BarRepository extends JpaRepository<Bar, Long> {
 @AllArgsConstructor
 class Bar {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	@OneToOne
@@ -71,7 +96,7 @@ interface FooRepository extends JpaRepository<Foo, Long> {
 @AllArgsConstructor
 class Foo {
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	@OneToOne(mappedBy = "foo")
