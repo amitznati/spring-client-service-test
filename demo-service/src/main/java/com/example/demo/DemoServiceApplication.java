43c251a7238a4997e54675b0b3c71c3f0ff2877f
AmitZnati <amitznati@gmail.com>
2018-05-10 22:20:53 +0200

    serive unify classes

--- demo-service/src/main/java/com/example/demo/DemoServiceApplication.java
+++ demo-service/src/main/java/com/example/demo/DemoServiceApplication.java
@@ -1,10 +1,23 @@
 package com.example.demo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class DemoServiceApplication {
@@ -13,20 +26,57 @@ public class DemoServiceApplication {
 		SpringApplication.run(DemoServiceApplication.class, args);
 	}
 	
-	@Component
-	public class ExposeEntityIdRestMvcConfiguration extends RepositoryRestConfigurerAdapter {
-
-	  @Override
-	  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-	    config.exposeIdsFor(Bar.class);
-	    config.exposeIdsFor(Foo.class);
-	  }
-	}
-	
 	
 }
 
+@Component
+class ExposeEntityIdRestMvcConfiguration extends RepositoryRestConfigurerAdapter {
 
+  @Override
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    config.exposeIdsFor(Bar.class);
+    config.exposeIdsFor(Foo.class);
+  }
+}
+
+@RepositoryRestResource(collectionResourceRel = "bars", path = "bars")
+interface BarRepository extends JpaRepository<Bar, Long> {
+
+}
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+class Bar {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	@OneToOne
+    @JoinColumn(name = "foo_id")
+    @RestResource(path = "barFoo", rel="foo")
+	private Foo foo;
+	
+}
+
+@RepositoryRestResource(collectionResourceRel = "foos", path = "foos")
+interface FooRepository extends JpaRepository<Foo, Long> {
+
+}
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+class Foo {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	@OneToOne(mappedBy = "foo")
+	private Bar bar;
+}
 
 
 
